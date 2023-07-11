bfcc8f83a951906e448172a6afa363e13fec59ec
TheWisePigeon <zozozozeph@gmail.com>
2023-07-11 10:56:46 +0000

    e-feat: upload media to cloudinary

--- index.ts
+++ index.ts
@@ -3,22 +3,43 @@ import { v2 as cloudinary } from "cloudinary"
 const PORT = process.env.PORT || 3000
 const { CLOUDINARY_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } = process.env
 cloudinary.config({
-    cloud_name:CLOUDINARY_NAME as string,
-    api_key:CLOUDINARY_API_KEY as string,
-    api_secret:CLOUDINARY_API_SECRET as string
+    cloud_name: CLOUDINARY_NAME as string,
+    api_key: CLOUDINARY_API_KEY as string,
+    api_secret: CLOUDINARY_API_SECRET as string
 })
 
 const server = Bun.serve({
-  port: PORT,
-  fetch: async(request)=> {
-    const form_data = await request.formData()
-    const file = form_data.get('file') as FileBlob
-    const file_extension = file.type.split("/")[1]
-    const media_id = crypto.randomUUID()
-    const file_name = `${media_id}.${file_extension}`
-    await Bun.write(file_name, file)
-    return new Response();
-  },
+    port: PORT,
+    fetch: async (request) => {
+        const form_data = await request.formData()
+        const file = form_data.get('file') as FileBlob
+        const file_extension = file.type.split("/")[1]
+        const media_id = crypto.randomUUID()
+        const file_name = `${media_id}.${file_extension}`
+        await Bun.write(file_name, file).catch(err => {
+            console.log(`Error writting file ${err}`)
+            return new Response("", { status: 500 })
+        })
+        try {
+            const upload_response = await cloudinary.uploader.upload(
+                file_name,
+                {
+                    public_id: media_id
+                },
+                (err, result) => {
+                    if (err) throw err
+                    return result
+                }
+            )
+            const { secure_url, url } = upload_response
+            return new Response(
+                JSON.stringify({ secure_url, url })
+            )
+        } catch (error) {
+            console.log(`Error uploading to cloudinary ${error}`)
+            return new Response("", { status:500 });
+        }
+    },
 });
 
 console.log(`Listening on localhost:${server.port}`);

f1fec6370877594c5e0edfb78464c41e51e2ad77
TheWisePigeon <zozozozeph@gmail.com>
2023-07-20 17:46:07 +0000

    add Dockerfile and config file

---
+++ config.ts
@@ -0,0 +1,4 @@
+export const IMAGEKIT_URL_ENDPOINT= process.env.IMAGEKIT_URL_ENDPOINT as string
+export const IMAGEKIT_PUBLIC_KEY= process.env.IMAGEKIT_PUBLIC_KEY as string
+export const IMAGEKIT_PRIVATE_KEY= process.env.IMAGEKIT_PRIVATE_KEY as string
+
